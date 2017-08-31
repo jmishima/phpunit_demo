b7752d11e7e949095f7df0a410b27031586f4de8
Juan Mishima <juan@cube.com.ar>
2017-08-31 19:35:01 -0300

    Addig test

---
+++ test/modules/app/DogTest.php
@@ -0,0 +1,22 @@
+<?php
+//require_once '../../../modules/app/Dog.php';
+
+class DogTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testMoveXPos() {
+		$oDog = new Dog(0,0);
+		$oDog->move(2,0);
+		$expected = 2;
+		$this->assertEquals($expected, $oDog->getXPos());
+	}
+
+	public function testInvalidArgument() {
+		$this->setExpectedException(InvalidArgumentException::class);
+		$oDog = new Dog('invalid pos x','invalid pos y');
+	}
+
+
+}
+
+?>
